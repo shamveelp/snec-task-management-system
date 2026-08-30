fcfbf0a27776d56e6b5a5075b01b075d45c175b8
Shamveel P <shamveelp007@gmail.com>
2026-08-30 22:28:16 +0530

    fix(google) - oauth from google setupped

--- backend/src/modules/auth/core/strategies/google.strategy.ts
+++ backend/src/modules/auth/core/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID') || 'dummy-client-id',
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET') || 'dummy-client-secret',
-      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL') || 'http://localhost:4000/api/v1/auth/google/callback',
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URI') || 'http://localhost:5000/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
